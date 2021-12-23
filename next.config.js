a605e1de2223c002a32805472b38c07ba35362d0
Sulav Baskota <sulav.baskota0419@gmail.com>
2021-12-23 23:13:39 +0545

    removed ignored files from git

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    TABLE_NAME: "project", 
-    DB_ACCESS_KEY_ID: "ASIATBIUXKSOC6PU6NKI",
-    DB_SECRET_ACCESS_KEY: "XYYOtuuhlRFP3LcLu5QxOo0fb1qRSwXbS8yN6HZc",
-    DB_SESSION_TOKEN: "FwoGZXIvYXdzEF8aDAwXZqf3w5z8xxr21yK9ASpdD2bBIxRCRGUVzz6eNJCZ1b7jW3xMMANl+4t0MbE9kOGlfGLMTUzp8OcmRpqxo9ddISGxy6I8ZtZRcdLpiSIp0pmsWFnixyvGyLz1T7VE3WMrS9I5LzNDjsAxTL4D1gbsrNlD9v3KrWSkNbEV/e4PezZMDccyVTPv1gLu8KqbNjqnXKk2pyrsN/tnm4JeI7/diHthuzQQAFFzHDvLIRzW8u7q8lBVAaPem2jM3CxBzI/0MCyY8sQaYeNsGijc8ZGOBjItc2b48xfJXfB2aRmGzqmvd+NIJU3IAnYhVRLqlkXtgtaSFIh8kczec7dsjwUD",
-  }
-}
